ebce94a3f0c78534941f24074a0744b9088fd11f
Shakil Ahmad <ahmad.shakil444@gmail.com>
2022-09-29 12:14:06 +0530

    day-2 by shakil

---
+++ frontend/src/Pages/UserPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from '../Components/Dashboard';
+import UserNavbar from '../Components/UserNavbar';
+
+
+const UserPage = () => {
+  return (
+	<>
+	<UserNavbar/>
+	<Dashboard/>
+	</>
+  )
+}
+
+export default UserPage
