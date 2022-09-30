28d7f43f801b5f05c948c1181dd4857dd3092907
Mohammadzaki0027 <zaki7875@gmail.com>
2022-09-30 19:44:25 +0530

    ui-part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,15 @@ import "./App.css";
 import { Box } from "@chakra-ui/react";
 import UserPage from "./Pages/UserPage";
 
-import SidebarRoutes from "./Planpage/Routes/SidebarRoutes";
+
 
 function App() {
   return (
-    // <Box>
-    //   <UserPage />
-    // </Box>
+    <Box>
+      <UserPage />
+    </Box>
 
-    <>
-<SidebarRoutes/>
-    
-    </>
+
   );
 }
 
