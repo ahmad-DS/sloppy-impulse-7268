c720f2c77d8539f127607ff9063b8f53d147adbf
akashbhoi2815 <manishbhoi1997@gmail.com>
2022-09-30 10:15:08 +0530

    fw17_0261_day-2

---
+++ frontend/src/Pages/HomePage/succeed/succeeddata.js
@@ -0,0 +1,29 @@
+export const cards={
+    Heading: 'What it takes to succeed',
+    data: [
+      {
+        imgUrl: 'https://s3.amazonaws.com/img.mynetdiary.com/images/cover-insights-Illustration-1@2x.png',
+        title: 'Find your Diet',
+        description:
+          'Find a diet that fits your lifestyle: Calorie Counting, Low-Carb, Keto, Vegan, Vegetarian and more.',
+      },
+      {
+        imgUrl: 'https://s3.amazonaws.com/img.mynetdiary.com/images/cover-insights-Illustration-2@2x.png',
+        title: 'Set Your Targets',
+        description:
+          'Choose your desired weekly weight loss rate, target date, and source of calories like fat, carbs, and proteins.',
+      },
+      {
+        imgUrl: 'https://s3.amazonaws.com/img.mynetdiary.com/images/cover-insights-Illustration-3@2x.png',
+        title: 'Log Your Meals & Exercise',
+        description:
+          'Use the App to track your food, physical activities, and nutrients. Use the barcode scanner. Log ahead of time for better planning and scheduling.',
+      },
+      {
+        imgUrl: 'https://s3.amazonaws.com/img.mynetdiary.com/images/cover-insights-Illustration-4@2x.png',
+        title: 'Reach Your Goals',
+        description:
+          'Monitor your daily progress, check your weight loss forecast, and receive ongoing advice and feedback. Learn more about nutrients in your diet and make better choices.',
+      },
+    ],
+  };
