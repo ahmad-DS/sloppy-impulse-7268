2989261b12a093de0bd20806352ca0c2cfdab323
Lalith Sai <lalithsai101@gmail.com>
2022-10-01 23:20:42 +0530

    Food page updated

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import Articles from '../HomePage/articles/Articles'
 import Compatible from '../HomePage/compatible/Compatible'
 import Counter from '../HomePage/Counter/Counter'
+import Footer from '../HomePage/footer/Footer'
 import LowerSection from '../HomePage/lowersection/LowerSection'
 import Succeed from '../HomePage/succeed/Succeed'
 import StorySlider from '../HomePage/SuccessStory/StorySlider'
 import UpperSection from '../HomePage/uppersection/UpperSection'
 import WomenHelthSection from '../HomePage/womenhealth/WomenHelthSection'
+import HomeNavbar from './HomeNavbar'
 
 const Home = () => {
   return (
     <div>
+      <HomeNavbar/>
       <UpperSection/>
       <WomenHelthSection/>
       <Succeed/>
@@ -19,6 +22,7 @@ const Home = () => {
       <Articles/>
       <Compatible/>
       <LowerSection/>
+      <Footer/>
     </div>
   )
 }
--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Login from "../Pages/authentication/Login";
+import Signup from "../Pages/authentication/Signup";
 import Food from "../Pages/Foodpage/Food";
+import Home from "../Pages/Home/Home";
 import Succeed from "../Pages/HomePage/succeed/Succeed";
 import UserPage from "../Pages/UserPage";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<UserPage />} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<UserPage />} />
       <Route path="/plan" element={<Food />} />
       <Route path="/food" element={<Food />} />
