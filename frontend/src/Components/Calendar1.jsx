d07d2f2fd5930f7a729f6119ee192d95b13bbeca
Shakil Ahmad <ahmad.shakil444@gmail.com>
2022-10-01 10:11:45 +0530

    calendar by shakil

--- frontend/src/Components/Calendar1.jsx
+++ frontend/src/Components/Calendar1.jsx
@@ -5,6 +5,8 @@ import { Heading, HStack, Image,Box,Button } from '@chakra-ui/react';
 
 function Calendar1() {
   const [date, setDate] = useState(new Date());
+  const [show,setShow]=useState("none");
+  
 
   const onChange = date => {
     console.log(typeof(date))
@@ -12,7 +14,7 @@ function Calendar1() {
   }
 
   return (
-    <HStack spacing={5}>
+    <HStack spacing={5} >
       <Box
       position="relative"
       >
@@ -24,10 +26,11 @@ function Calendar1() {
           background: 'none',
           cursor: "pointer"
         }}
+        onClick={()=>{setShow("block")}}
         src="https://www.mynetdiary.com/img/icon/date_range.png"
       />
-      <Box>
-        <Button>
+      <Box position="absolute" zIndex={5} shadow='xl' borderWidth='1px' display={show}>
+        <Button onClick={()=>{setShow("none")}}>
           close
         </Button>
         <Calendar onChange={onChange} value={date}/>
@@ -38,7 +41,7 @@ function Calendar1() {
         w="14px"
         src="https://s3.amazonaws.com/img.mynetdiary.com/left.png"
       />
-      <Heading fontSize={25} color="white">{date.toString().split(" ")[0]},{date.toString().split(" ")[1]}</Heading>
+      <Heading fontSize={25} color="white">{date.toString().split(" ")[0]},{date.toString().split(" ")[1]} {date.toString().split(" ")[2]}</Heading>
       <Image
         w="14px"
         src="https://s3.amazonaws.com/img.mynetdiary.com/right.png"
---
+++ frontend/src/Redux/userReducer/reducer.js
@@ -0,0 +1,4 @@
+const initState={
+	current_date:new Date(),
+	
+}
