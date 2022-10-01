4ff40f56473480a254cadd178e3632d8f2326dfd
Lalith Sai <lalithsai101@gmail.com>
2022-10-01 18:34:22 +0530

    calories added

--- frontend/src/Pages/Foodpage/Apple.jsx
+++ frontend/src/Pages/Foodpage/Apple.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
 
-const Apple = () => {
+const Apple = ({ val }) => {
   return (
     <div>
-      <CircularProgress value={40} color="green.400">
-        <CircularProgressLabel>40%</CircularProgressLabel>
+      <CircularProgress
+        size="250px"
+        thickness="3px"
+        value={40}
+        color="green.400"
+      >
+        <CircularProgressLabel fontSize="20px">{val}</CircularProgressLabel>
       </CircularProgress>
     </div>
   );
